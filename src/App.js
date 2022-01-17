622aa9851f52eae8c5e2fff753c1d240686598d1
Wangdi Tenzing <wangdi457@gmail.com>
2022-01-17 10:58:37 -0500

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 //feature 1
 
 import React, { useState, useEffect } from "react";
+import Filter from "./components/Filter/Filter";
 import Products from "./components/Products/Products";
 import data from "./data.json";
 
 function App() {
-  const products = data.products;
+  const [productState, setProductState] = useState(data.products);
   const [size, setSize] = useState("");
   const [sort, setSort] = useState("");
 
-  useEffect(() => {
-    console.log(products);
-  }, [products]);
+  const filterProducts = (event) => {
+    if (event.target.value === " ") {
+      setSize(event.target.value);
+      setProductState(productState);
+    } else {
+      setSize(event.target.value);
+
+      console.log("productState", productState);
+      setProductState(
+        productState.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) > -1
+        )
+      );
+    }
+  };
+  const sortProducts = (event) => {
+    const sort = event.target.value;
+    setSort(sort);
+    setProductState(
+      productState
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        )
+    );
+  };
+
   return (
     <div className="grid-container">
       <header>
@@ -20,7 +55,14 @@ function App() {
       <main>
         <div className="content">
           <div className="main">
-            <Products products={products} />
+            <Filter
+              count={productState.length}
+              size={productState.size}
+              sort={productState.sort}
+              filterProducts={filterProducts}
+              sortProducts={sortProducts}
+            />
+            <Products products={productState} />
           </div>
           <div className="sidebar">Cart Items</div>
         </div>
