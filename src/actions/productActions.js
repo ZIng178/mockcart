5e016189e33b9f5840ca199c2bcf0778e339c964
Wangdi Tenzing <wangdi457@gmail.com>
2022-02-20 16:10:08 -0500

    added headers

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -5,7 +5,12 @@ import {
 } from "../type";
 
 export const fetchProducts = () => async (dispatch) => {
-  const res = await fetch("/api/products");
+  const res = await fetch("/api/products", {
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+  });
   const data = await res.json();
   console.log("DATA", data);
   dispatch({
