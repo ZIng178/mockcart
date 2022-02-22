a2d118dd3924698f07d12d75569e7ab0042ad982
Wangdi Tenzing <wangdi457@gmail.com>
2022-02-22 09:49:26 -0500

    added .env file

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.post("/api/orders", async (req, res) => {
   res.send(order);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server is running at ${port}`);
 });
